8d2df48c3eafc36d829851bfc3e803b70d161f44
v-tyjh <47658264+v-tyjh@users.noreply.github.com>
2020-04-17 14:37:23 +0200

    Add files via upload

--- créercompte.php
+++ créercompte.php
@@ -4,7 +4,7 @@
 $database = "utilisateurs";
 //connectez-vous dans votre BDD
 //Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
-$db_handle = mysqli_connect('localhost', 'root', '' );
+$db_handle = mysqli_connect('localhost:3307', 'root', '' );
 $db_found = mysqli_select_db($db_handle, $database);
 
 
@@ -12,7 +12,7 @@ $prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
 $nom = isset($_POST["nom"])? $_POST["nom"] : "";
 $adresse = isset($_POST["adresse"])? $_POST["adresse"] : "";
 $email = isset($_POST["email"])? $_POST["email"] : "";
-$mdp = isset($_POST["mdp"])? $_POST["mdp"] : "";
+
 $paiement = isset($_POST["paiement"])? $_POST["paiement"] : "";
 $doc = isset($_POST["doc"])? $_POST["doc"] : "";
 
@@ -26,16 +26,52 @@ $doc = isset($_POST["doc"])? $_POST["doc"] : "";
  $erreur .= "adresse est vide. <br>"; }
  if ($email == "") {
  $erreur .= "email est vide. <br>"; }
-  if ($mdp == "") {
- $erreur .= "mdp est vide. <br>"; }
+
  if ($paiement == "") {
  $erreur .= "paiement est vide. <br>"; }
 if ($doc == "") { 
     $erreur .= "le doc n'est pas coché. <br>"; }
 
  if ($erreur == "") {
- header('Location: http://localhost/projet%20page%20web/postconnexion.html');
-  exit();
+	    if (isset($_POST['button0'])) {
+if ($db_found) {
+$sql = "SELECT * FROM acheteurs";
+if ($nom != "") {
+$sql .= " WHERE nom LIKE '%$nom%'";
+if ($email != "") {
+$sql .= " AND Email LIKE '%$email%'";
+}
+}
+$result = mysqli_query($db_handle, $sql);
+if (mysqli_num_rows($result) != 0) {
+echo "compte already exists. Duplicate not allowed.";
+} else {
+$sql = "INSERT INTO acheteurs(Prenom, Nom, Email, Adresse )
+ VALUES('$prenom', '$nom', '$email', '$adresse')";
+$result = mysqli_query($db_handle, $sql);
+echo "Add successful. <br>";
+$sql = "SELECT * FROM vendeurs";
+if ($nom != "") {
+$sql .= " WHERE Pseudo LIKE '%$nom%'";
+if ($email != "") {
+$sql .= " AND Email LIKE '%$email%'";
+}
+}
+$result = mysqli_query($db_handle, $sql);
+
+echo "ID: " . $prenom . "<br>";
+echo "Titre: " . $email . "<br>";
+echo "Titre: " . $nom . "<br>";
+echo "Auteur: " . $email . "<br>";
+
+}
+} else {
+echo "Database not found";
+}
+///header('Location: http://localhost/projet%20page%20web/postconnexion.html');
+ // exit();
+}
+ 
  }
  else {
  echo "Erreur :". $erreur. '<br>';
--- vendeurcréercompte.php
+++ vendeurcréercompte.php
@@ -4,13 +4,14 @@
 $database = "utilisateurs";
 //connectez-vous dans votre BDD
 //Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
-$db_handle = mysqli_connect('localhost', 'root', '' );
+$db_handle = mysqli_connect('localhost:3307', 'root', '' );
 $db_found = mysqli_select_db($db_handle, $database);
 
 
 $pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
 $nom = isset($_POST["nom2"])? $_POST["nom2"] : "";
 $email = isset($_POST["email2"])? $_POST["email2"] : "";
+$photo = isset($_POST["photo"])? $_POST["photo"] : "";
 
 
  $erreur = "";
@@ -24,10 +25,47 @@ $email = isset($_POST["email2"])? $_POST["email2"] : "";
 
 
  if ($erreur == "") {
- header('Location: http://localhost/projet%20page%20web/vendeurcompte.html');
-  exit();
+ 
   ///récuperer les info et les mettre dans la bdd
- }
+   if (isset($_POST['button0'])) {
+if ($db_found) {
+$sql = "SELECT * FROM vendeurs";
+if ($pseudo != "") {
+$sql .= " WHERE Pseudo LIKE '%$pseudo%'";
+if ($email != "") {
+$sql .= " AND Email LIKE '%$email%'";
+}
+}
+$result = mysqli_query($db_handle, $sql);
+if (mysqli_num_rows($result) != 0) {
+echo "Book already exists. Duplicate not allowed.";
+} else {
+$sql = "INSERT INTO vendeurs(Pseudo, Nom, Email ) VALUES('$pseudo', '$nom', '$email')";
+$result = mysqli_query($db_handle, $sql);
+echo "Add successful. <br>";
+$sql = "SELECT * FROM vendeurs";
+if ($pseudo != "") {
+$sql .= " WHERE Pseudo LIKE '%$pseudo%'";
+if ($email != "") {
+$sql .= " AND Email LIKE '%$email%'";
+}
+}
+$result = mysqli_query($db_handle, $sql);
+
+echo "ID: " . $pseudo . "<br>";
+echo "Titre: " . $nom . "<br>";
+echo "Auteur: " . $email . "<br>";
+
+}
+}
+} else {
+echo "Database not found";
+}
+}
+
+ ///header('Location: http://localhost/projet%20page%20web/vendeurcompte.html');
+ // exit();
+ 
  else {
  echo "Erreur :<br>". $erreur. '<br>';
  echo "Vous devez remplir tous les champs afin de vous créer un compte.";
