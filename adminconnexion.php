97758137a97cdb5437f700c8c90dd8025eb4d126
v-tyjh <47658264+v-tyjh@users.noreply.github.com>
2020-04-17 14:58:40 +0200

    Add files via upload

--- adminconnexion.php
+++ adminconnexion.php
@@ -4,11 +4,13 @@
 $database = "utilisateurs";
 //connectez-vous dans votre BDD
 //Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
-$db_handle = mysqli_connect('localhost', 'root', '' );
+$db_handle = mysqli_connect('localhost:3307', 'root', '' );
 $db_found = mysqli_select_db($db_handle, $database);
 
 
-$pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
+$nom = isset($_POST["nom"])? $_POST["nom"] : "";
+$id = isset($_POST["ID"])? $_POST["ID"] : "";
+$prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
 $email = isset($_POST["email3"])? $_POST["email3"] : "";
 
 
@@ -16,12 +18,48 @@ $email = isset($_POST["email3"])? $_POST["email3"] : "";
 
  if ($email == "") {
  $erreur .= "email est vide. <br>"; }
-  if ($pseudo == "") {
+  if ($prenom == "") {
  $erreur .= "pseudo est vide. <br>"; }
 
  if ($erreur == "") {
-   header('Location: http://localhost/projet%20page%20web/admincompte.html');
-  exit();
+	   ///récuperer les info et les mettre dans la bdd
+   if (isset($_POST['button0'])) {
+if ($db_found) {
+$sql = "SELECT * FROM admin";
+if ($prenom != "") {
+$sql .= " WHERE Pseudo LIKE '%$prenom%'";
+if ($email != "") {
+$sql .= " AND Email LIKE '%$email%'";
+}
+}
+$result = mysqli_query($db_handle, $sql);
+if (mysqli_num_rows($result) != 0) {
+echo "Book already exists. Duplicate not allowed.";
+} else {
+$sql = "INSERT INTO admin(Nom, ID , Prenom, Email ) VALUES('$nom', '$id', '$email', '$prenom')";
+$result = mysqli_query($db_handle, $sql);
+echo "Add successful. <br>";
+$sql = "SELECT * FROM admin";
+if ($prenom != "") {
+$sql .= " WHERE Pseudo LIKE '%$prenom%'";
+if ($email != "") {
+$sql .= " AND Email LIKE '%$email%'";
+}
+}
+$result = mysqli_query($db_handle, $sql);
+
+echo "ID: " . $nom . "<br>";
+echo "Titre: " . $prenom . "<br>";
+echo "Auteur: " . $email . "<br>";
+echo "Auteur: " . $id . "<br>";
+
+}
+}
+} else {
+echo "Database not found";
+}
+   ///header('Location: http://localhost/projet%20page%20web/admincompte.html');
+ /// exit();
  }
  else {
  echo "Erreur :<br>". $erreur. '<br>';
